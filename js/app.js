96555491a06909e4ad86a873250ae15250a4d88c
billal-fahmid <billalcooom@gmail.com>
2023-02-18 13:00:27 +0600

    all cards random backgound color set done

--- js/app.js
+++ js/app.js
@@ -77,3 +77,18 @@ for(let i=0 ; i< cards.length ; i++){
 document.getElementById('card-1').addEventListener('mouseenter', function(){
     setRandomBackgoundColor('card-1');
 })
+document.getElementById('card-2').addEventListener('mouseenter',function(){
+    setRandomBackgoundColor('card-2');
+})
+document.getElementById('card-3').addEventListener('mouseenter',function(){
+    setRandomBackgoundColor('card-3');
+})
+document.getElementById('card-4').addEventListener('mouseenter',function(){
+    setRandomBackgoundColor('card-4');
+})
+document.getElementById('card-5').addEventListener('mouseenter',function(){
+    setRandomBackgoundColor('card-5');
+})
+document.getElementById('card-6').addEventListener('mouseenter',function(){
+    setRandomBackgoundColor('card-6');
+})
