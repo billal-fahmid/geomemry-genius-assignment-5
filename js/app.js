9b656ae736d6c0d4d82c1c03e1ffed5b91279ee3
billal-fahmid <billalcooom@gmail.com>
2023-02-18 20:29:55 +0600

    new random-bg.js create for generate reandom bg color

--- js/app.js
+++ js/app.js
@@ -74,21 +74,3 @@ for(let i=0 ; i< cards.length ; i++){
     // cards[i].style.hover:backgroundColor ='#'+Math.floor(Math.random()*16777215).toString(16);
     document.getElementsByClassName
 }
-document.getElementById('card-1').addEventListener('mouseenter', function(){
-    setRandomBackgoundColor('card-1');
-})
-document.getElementById('card-2').addEventListener('mouseenter',function(){
-    setRandomBackgoundColor('card-2');
-})
-document.getElementById('card-3').addEventListener('mouseenter',function(){
-    setRandomBackgoundColor('card-3');
-})
-document.getElementById('card-4').addEventListener('mouseenter',function(){
-    setRandomBackgoundColor('card-4');
-})
-document.getElementById('card-5').addEventListener('mouseenter',function(){
-    setRandomBackgoundColor('card-5');
-})
-document.getElementById('card-6').addEventListener('mouseenter',function(){
-    setRandomBackgoundColor('card-6');
-})
---
+++ js/random-bg.js
@@ -0,0 +1,18 @@
+document.getElementById('card-1').addEventListener('mouseenter', function(){
+    setRandomBackgoundColor('card-1');
+})
+document.getElementById('card-2').addEventListener('mouseenter',function(){
+    setRandomBackgoundColor('card-2');
+})
+document.getElementById('card-3').addEventListener('mouseenter',function(){
+    setRandomBackgoundColor('card-3');
+})
+document.getElementById('card-4').addEventListener('mouseenter',function(){
+    setRandomBackgoundColor('card-4');
+})
+document.getElementById('card-5').addEventListener('mouseenter',function(){
+    setRandomBackgoundColor('card-5');
+})
+document.getElementById('card-6').addEventListener('mouseenter',function(){
+    setRandomBackgoundColor('card-6');
+})
